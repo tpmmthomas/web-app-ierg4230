1e83331727c728c552077eea541cce691e173bc7
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2021-12-09 22:06:04 +0800

    sdf

--- database.py
+++ database.py
@@ -2,9 +2,12 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+global db
+
 
 def setup_db(app):
+    global db
+    db = SQLAlchemy(app)
     database_name ='local_db_name'
     default_database_path= "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', database_name)
     database_path = os.getenv('DATABASE_URL', default_database_path)
--- manager.py
+++ manager.py
@@ -1,9 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import app
 from database import db
 
-def runmigrate():
+def runmigrate(app):
     migrate = Migrate(app, db)
     manager = Manager(app)
     manager.add_command('db', MigrateCommand)
